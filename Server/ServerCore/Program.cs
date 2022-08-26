95161c9f1677789bd56fa396fe6f27774073d1d6
cho youngmin <lukechodev@gmail.com>
2022-08-26 15:56:59 +0900

    C# - Mutual exclusive
    Tested Monitor class & lock keyword

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -8,6 +8,7 @@ namespace ServerCore
     internal class Program
     {   
         static int number = 0;
+        static object _obj = new object();
 
         static void Thread_1()
         {
@@ -26,7 +27,37 @@ namespace ServerCore
                 */
 
                 // 원자성 보장 동작을 하고 싶으면 Interlock 을 사용할 수 있음. 
-                Interlocked.Increment(ref number);
+                //Interlocked.Increment(ref number);
+
+                // Mutual Exclusive (상호배제)
+                // 간단하게 변수하나의 값을 대입하는 등의 연산이 아니라 다양한 연산을 해야한다면 Monitor 로 상호배제를 시켜줄 수 있다.
+                Monitor.Enter(_obj);
+                {
+                    number++;
+
+                    //return; // 실수로 상호배제 시킨 오브젝트를 해제시켜주지 않았다면 해당 변수에 접근하는 다른 쓰레드가 무한대기상태에 빠지는데
+                    // 이를 데드락( Dead lock ) 이라고 함.
+                }
+                Monitor.Exit(_obj);
+
+                // 데드락 방지를 위한 방법은 
+                // 1. try 사용
+                try
+                {
+                    Monitor.Enter(_obj);
+                    number++;
+                }
+                finally
+                {
+                    Monitor.Exit(_obj);
+                }
+
+                // 2. lock 키워드 사용
+                // lock 은 내부적으로 Monitor.Enter, Exit 으로 구성되어있고 Exit 호출을 보장하는 키워드.
+                lock(_obj)
+                {
+                    number++;
+                }
             }
         }
 
@@ -37,10 +68,33 @@ namespace ServerCore
 
             for (int i = 0; i < 10000; i++)
             {
-                int temp = number;
-                temp -= 1;
-                number = temp;
+                //int temp = number;
+                //temp -= 1;
+                //number = temp;
+
+                //Interlocked.Decrement(ref number);
+                Monitor.Enter(_obj);
+
+                number--;
+
+                Monitor.Exit(_obj);
+
+                try
+                {
+                    Monitor.Enter(_obj);
+                    number--;
+                }
+                finally
+                {
+                    Monitor.Exit(_obj);
+                }
+
+                lock (_obj)
+                {
+                    number--;
+                }
             }
+
         }
         static void Main(string[] args)
         {
