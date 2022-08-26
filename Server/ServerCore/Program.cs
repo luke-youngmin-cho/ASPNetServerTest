fab5e2557a5cf47b1a32548b88af60db3c6af460
cho youngmin <lukechodev@gmail.com>
2022-08-26 14:01:14 +0900

    C# - Optimize problem
    Tested multi-thread problem with compiler's optimization

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -6,55 +6,47 @@ namespace ServerCore
 {
     internal class Program
     {
-        static void MainThread(object state)
+
+        // 디버그모드에서는 정상작동 하지만
+        // 릴리스모드에서는 컴파일러가 코드최적화를 하는 과정에서 동작이 달라짐 (volatile을 사용하지 않았을 경우)
+        volatile static private bool _stop = false;
+        static private void ThreadMain()
         {
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine("쓰레드 시작");
+
+            // 디스어셈블리를 들여다보면 
+            // 컴파일러가 컴파일을할 때 최적화하기 위해서 
+            // 
+            // if (_stop == false)
+            // while (true) {} 
+            // 
+            // 형태로 컴파일을 하는데, 멀티쓰레딩으로 외부 스레드에서 while 조건을 변경하고싶은 경우 
+            // 이런 최적화는 오작동을 일으킴. 
+            // 이럴때 사용할 수 있는조건이 volatile 키워드.
+            // volatile 키워드는 최적화를 하지 않도록하는 키워드
+            // C#의 volatile은 C++ 의 volatile 과 의미가 달라서 사용하지 않는것을 권함. 
+            // 그래서 lock 등으로 해결함
+            while (_stop == false)
             {
-                Console.WriteLine("Hello Thread");
-            }   
+
+            }
+
+            Console.WriteLine("쓰레드 종료");
         }
+
         static void Main(string[] args)
         {
+            Task t = new Task(ThreadMain);
 
-            // C# Thread는 기본적으로 forground 실행
-            // forground 는 main thread 와 상관없이 종료될떄까지 실행됨
-            // background 는 main thread 가 종료될때 같이 종료됨
-            // Thread 를 많이 만든다고해서 효율이 비례하는것은 아님
+            t.Start();
+            Thread.Sleep(1000);
+            _stop = true;
 
-            //Thread t = new Thread(MainThread);
-            //t.Name = "Test Thread";
-            //t.IsBackground = true;
-            //t.Start();
-            //Console.WriteLine("Wait for thread started");
-            //t.Join();
-            //Console.WriteLine("Hello world");
 
-            // ThreadPooling
-            // ThreadPool 을 사용할 떄는, 가급적 적은 연산을 수행하도록 하는것이 좋음
-            // 과도한 연산이 부과되면 / 과도한 쓰레드를 사용하면 ThreadPool 이 중단될 수 있다.
-            // 방지하기 위한 방법으로는 Min, Max thread 지정해주는방법과 Task를 이용하는 방법이 있다.
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
-            for (int i = 0; i < 5; i++)
-                ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
-
-            ThreadPool.QueueUserWorkItem(MainThread);
-
-            // Task
-            // Task 도 ThreadPool 을 사용하게됨
-            // 연산량이 많다면 LongRunning 옵션 설정을 해 줄수있다.
-            // LongRunning 옵션은 해당 Thread 를 별도로 관리하도록하고, ThreadPool 의 Min, Max 설정과 무관하게 관리된다.
-            for (int i = 0; i < 5; i++)
-            {
-                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
-                t.Start();
-
-            }
-
-            while (true)
-            {
-
-            }
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
+            t.Wait();
+            Console.WriteLine("종료 성공");
         }
     }
 }
