909525760a641cdaa9192b326fcb7246a1b6c389
cho youngmin <lukechodev@gmail.com>
2022-09-02 11:51:48 +0900

    C# - Mutex
    Tested Mutex (for kernel level synchronization)

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -2,40 +2,21 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-// Auto reset event
-//
-// spin lock 보다 훨씬 느림 (커널 레벨에서 체크하기때문에)
-// 일반적으로 사용하지 않음.
-
+// Mutex
+// 커널레벨동기화 객체
 namespace ServerCore
 {
-    class MyLock
-    {
-        AutoResetEvent _available = new AutoResetEvent(true);
-
-        public void Acquire()
-        {
-            _available.WaitOne(); // _available.Reset() 은 WaitOne 에서 양자적으로 실행하므로 별도호출 필요없음
-            // AutoResetEvent 대신 ManualResetEvent 를 쓴다면 WaitOne() 후에 Reset() 호출해야함
-        }
-
-        public void Release()
-        {
-            _available.Set();
-        }
-    }
-
     internal class Program
     {
         static int _num = 0;
-        static MyLock _lock = new MyLock();
+        static Mutex _lock = new Mutex();
         static void Thread_1()
         {
             for (int i = 0; i < 10000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num++;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
@@ -43,9 +24,9 @@ namespace ServerCore
         {
             for (int i = 0; i < 10000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num--;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
         static void Main(string[] args)
