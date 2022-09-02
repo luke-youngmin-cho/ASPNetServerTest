129a882d3e52d098cdeaa51b9aa30e40ab00a16b
cho youngmin <lukechodev@gmail.com>
2022-09-02 12:55:43 +0900

    C# - lock, spinlock, readerwriter lock
    Compared lock vs spinlock vs readerwriter lock

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -2,42 +2,51 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-// Mutex
-// 커널레벨동기화 객체
 namespace ServerCore
 {
     internal class Program
     {
-        static int _num = 0;
-        static Mutex _lock = new Mutex();
-        static void Thread_1()
+        static object _lock = new object();
+        static SpinLock _lock2 = new SpinLock();
+        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
+        static bool _lockTaken;
+        class Reward
         {
-            for (int i = 0; i < 10000; i++)
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
         }
 
-        static void Thread_2()
+        static Reward GetRewardById(int id)
         {
-            for (int i = 0; i < 10000; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
+            _lock3.EnterReadLock();
+            _lock3.ExitReadLock();
+            return null;
         }
+
+        static void AddReward(Reward reward)
+        {
+            // lock
+            lock (_lock)
+            {
+
+            }
+
+            // spin lock
+            try
+            {
+                _lock2.Enter(ref _lockTaken);
+            }
+            finally
+            {
+                if (_lockTaken)
+                    _lock2.Exit();
+            }
+
+            // reader writer lock
+            _lock3.EnterWriteLock();
+            _lock3.ExitWriteLock();
+        }
+
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-            Console.WriteLine(_num);
         }
     }
 }
