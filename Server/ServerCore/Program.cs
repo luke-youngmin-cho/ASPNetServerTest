98a57a52b605b491a19091b35660f7db888a2444
cho youngmin <lukechodev@gmail.com>
2022-09-02 16:37:33 +0900

    C# - TLS (Thread Local Storage)
    Tested ThreadLocal

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -6,37 +6,31 @@ namespace ServerCore
 {
     internal class Program
     {
-        static volatile int count = 0;
-        static Lock _lock = new Lock();
+        // TLS ( Thread Local Storage )
+        // 쓰레드들이 하나의 Lock 에 몰려서 연산이 지연되는 문제를 해결하기위해 
+        // 쓰레드들을 일정량 묶어서 스택영역에 가져와서 분배 처리 하는 방법
+        // MMORPG 같은경우에 많은 클라이언트가 게임로직에 대한 자원에 전부 접근하기떄문에 
+        // TLS 가 해결방법으로 중요하게 사용된다.
+        static ThreadLocal<string> ThreadLocal = new ThreadLocal<string>(() =>
+        { 
+            return $"My name is {Thread.CurrentThread.ManagedThreadId}";
+        });
+        static void WhoAmI()
+        {
+            bool repeat = ThreadLocal.IsValueCreated;
+            if (repeat)
+                Console.WriteLine(ThreadLocal.Value + "{repeat}");
+            else
+                Console.WriteLine(ThreadLocal.Value);
+        }
         static void Main(string[] args)
         {
-            Task t1 = new Task(delegate ()
-            {
-                for (int i = 0; i < 100000; i++)
-                {
-                    _lock.WriteLock();
-                    count++;
-                    _lock.WriteUnLock();
-                }
-            });
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            // 인자로 넘겨준 함수를 스레드풀에서 가져온 각 스레드에 할당해서 호출해주는 함수
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
 
-            Task t2 = new Task(delegate ()
-            {
-                for (int i = 0; i < 100000; i++)
-                {
-                    _lock.WriteLock();
-                    count--;
-                    _lock.WriteUnLock();
-                }
-            });
-
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-
-
-            Console.WriteLine(count);
+            ThreadLocal.Dispose();
         }
     }
 }
